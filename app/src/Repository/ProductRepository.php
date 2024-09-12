e44b2f14e33c29385f445631478f5b977673a351
Andreas Todt <andreas.todt@me.com>
2024-09-12 10:55:19 -0600

    v0.4.0 added data model

---
+++ app/src/Repository/ProductRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @extends ServiceEntityRepository<Product>
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry, private LoggerInterface $logger)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * @return Product Returns a Product object with the given ID or NULL.
+     */
+    public function findById($value): mixed
+    {
+        $product = $this->createQueryBuilder('c')
+            ->andWhere('c.id = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        if (!$product instanceof Product) {
+            return null;
+        }
+
+        return $product;
+    }
+
+    public function createProduct(Product $product): Product
+    {
+        $this->logger->info('Creating new product');
+        $this->getEntityManager()->persist($product);
+        $this->getEntityManager()->flush();
+
+        return $product;
+    }
+
+    public function removeProduct(Product $product): void
+    {
+        $this->logger->info('Deleting product ' . $product->getId());
+        $this->getEntityManager()->remove($product);
+        $this->getEntityManager()->flush();
+    }
+
+    public function saveProduct(Product $product): Product
+    {
+        $this->logger->info('Deleting product ' . $product->getId());
+        $this->getEntityManager()->persist($product);
+        $this->getEntityManager()->flush();
+
+        return $product;
+    }
+
+
+}
